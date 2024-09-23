9faa992843c625225b78b4e6606f12255c20ef2f
KnigaMichail <148048511+KnigaMichail@users.noreply.github.com>
2024-09-23 11:32:05 +0500

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,28 @@
-var button = document.querySelection('button');
+//var button = document.querySelection('button');
 
-function showPic1() {
-    document.write('<img scr="/img/1697512153350.png">');
-}
+//function showPic1() {
+   // document.write('<img scr="/img/1697512153350.png">');
+//}
+
+//$(document).ready(function($) {
+	
+	//Открытие окна по клику на кнопку
+	//$('.popup-open').click(function(e) {
+	//	$('.popup-overlay').toggleClass('open'),
+	//	$('body').toggleClass('popup-hidden');
+	//});
+	
+	//Закрытие окна по клику на кнопку
+	//$('.popup-close').click(function() {
+	//	$('.popup-overlay').removeClass('open');
+	//});
+	
+	//Закрытие окна по клику вне окна
+	//$(".popup-overlay").on('click', function(e) {
+    //	if (!$(e.target).closest(".popup").length) {
+     // 	$(this).removeClass('open'),
+	//	$('body').removeClass('popup-hidden');
+    //	} e.stopPropagation();
+	//});
+	
+//});
